62380862c1ce91f9006825d6ac5fb8525ceea0f0
manishchh <81131446+manishchh@users.noreply.github.com>
2024-06-09 17:50:43 +0930

    Refactor DiseaseStrategy clas to return AlertLevel instead of directly setting it in the Patient

--- DiseaseStrategy.h
+++ DiseaseStrategy.h
@@ -1,14 +1,14 @@
 #ifndef DISEASE_STRATEGY_H
 #define DISEASE_STRATEGY_H
 
+class Patient;
+class Vitals;
 #include "PatientAlertLevels.h"
-#include "Vitals.h"
-#include "Patient.h"
 
 class DiseaseStrategy {
 public:
     virtual ~DiseaseStrategy() = default;
-    virtual const void calculateAlertLevel(const Vitals* v, Patient* p) = 0;
+    virtual AlertLevel calculateAlertLevel(const Vitals& v, Patient& p) = 0;
 };
 
 #endif
